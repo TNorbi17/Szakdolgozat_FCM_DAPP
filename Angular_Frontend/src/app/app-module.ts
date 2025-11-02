08ad103376d6f7a416f35252b8afd847f25eca69
Tóth Norbert <tothnorbi2004@gmail.com>
2025-11-02 21:15:51 +0100

    Kezdőoldal létrehozás

--- Angular_Frontend/src/app/app-module.ts
+++ Angular_Frontend/src/app/app-module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SessionService } from './services/blockchain/services/blockchain.service';
 import { Footer } from './components/footer/footer';
+import { Home } from './components/home/home';
 @NgModule({
   declarations: [
     App,
-    Footer
+    Footer,
+    Home
   ],
   imports: [
     BrowserModule,
