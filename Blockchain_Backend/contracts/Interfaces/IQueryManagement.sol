e3300d42f73bf87c9b01ed5ff1cdd74201fc5be2
Tóth Norbert <tothnorbi2004@gmail.com>
2025-11-05 18:46:47 +0100

    Bejelentkezés funkció backend kiegészítés

---
+++ Blockchain_Backend/contracts/Interfaces/IQueryManagement.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "../Types.sol";
+interface IQueryManagement {
+    function getUserByAddress(address _walletAddress)
+        external
+        view
+        returns (
+            address,
+            Enums.UserType,
+            string memory,
+            string memory,
+            string memory,
+            uint256
+        );
+
+    function getTeamByName(string memory _teamName)
+        external
+        view
+        returns (
+            uint256,
+            string memory,
+            uint256,
+            address,
+            string memory,
+            uint256,
+            address[] memory
+        );
+
+    function getPlayerByName(string memory _playerName)
+        external
+        view
+        returns (
+            uint256,
+            string memory,
+            Enums.PlayerPosition,
+            uint256,
+            address,
+            string memory,
+            string memory,
+            uint256,
+            bool,
+            uint256
+        );
+}
